4bce71cc013c90e01d6ba092a00bcedd1d07bc3f
admin <admin@admin.com>
2026-06-11 17:32:26 +0900

    댓글 기능 추가

--- board_dao.py
+++ board_dao.py
@@ -1,22 +1,64 @@
 import pymysql
 
+# DB CRUD
+
+# conn    = 데이터베이스 연결 객체
+# cursor  = SQL 실행 객체
+
 class BoardDAO:
-    
+       
     def __init__(self):
+
         self.host = "localhost"
         self.user = "board_user"
         self.password = "board1234"
         self.database = "board_db"
 
     def get_connection(self):
+
         return pymysql.connect(
-            host = self.host,
-            user = self.user,
-            password = self.password,
-            database = self.database,
-            charset = "utf8mb4")
-    
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database,
+            charset="utf8mb4"
+        )
+       
+    def insert_board(self, title, content, writer):
+
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+        INSERT INTO board
+        (title, content, writer)
+        VALUES
+        (%s, %s, %s)
+        """
+        
+        # sql = f"""
+        #     INSERT INTO board
+        #     (title, content, writer)
+        #     VALUES
+        #     ('{title}', '{content}', '{writer}')
+        #     """
+
+        # f-string → 출력문, 로그용
+        # %s 파라미터 바인딩 → SQL용
+        
+        cursor.execute(
+            sql,
+            (title, content, writer)
+        )
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        print("등록 완료")
+
     def select_all(self):
+
         conn = self.get_connection()
         cursor = conn.cursor()
 
@@ -32,3 +74,128 @@ class BoardDAO:
         conn.close()
 
         return result
+
+    def select_one(self, board_id):
+
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+        SELECT *
+        FROM board
+        WHERE id=%s
+        """
+
+        cursor.execute(sql, (board_id,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        return result
+    
+    def update_board(self, board_id, title, content):
+
+        conn =  self.get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+        UPDATE board
+        SET title=%s,
+            content=%s
+        WHERE id=%s
+        """
+
+        cursor.execute(
+            sql,
+            (title, content, board_id)
+        )
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    def insert_comment(self, board_id, content, writer):
+
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+        INSERT INTO comment
+        (board_id, content, writer)
+        VALUES
+        (%s, %s, %s)
+        """
+
+        cursor.execute(
+            sql,
+            (board_id, content, writer)
+        )
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    def select_comments(self, board_id):
+
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+        SELECT *
+        FROM comment
+        WHERE board_id=%s
+        ORDER BY id ASC
+        """
+
+        cursor.execute(
+            sql,
+            (board_id,)
+        )
+
+        result = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return result
+    
+    def delete_comment(self, comment_id):
+
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+        DELETE
+        FROM comment
+        WHERE id=%s
+        """
+
+        cursor.execute(sql, (comment_id,))
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    def delete_board(self, board_id):
+
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+        DELETE
+        FROM board
+        WHERE id=%s
+        """
+
+        cursor.execute(sql, (board_id,))
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        print("삭제 완료")
+
--- main.py
+++ main.py
@@ -2,28 +2,151 @@ from board_dao import *
 
 board_dao = BoardDAO()
 
-# # 커넥션 테스트
-# board_dao.get_connection()
-
 while True:
-    
+
     print("=" * 40)
-    print("1.목록  2.등록  3.내용  4.삭제  0.종료")
+    print("1.글 목록  2.글 등록  3.글 조회  4.글 삭제  5.글 수정  6.댓글 등록  7.댓글 삭제  0.종료")
     print("=" * 40)
 
     menu = input("선택 > ")
 
-    if menu == "0":
-        break
-    elif menu == "1": #게시판 select * from board
-        boards = n=board_dao.select_all()
-        print(boards)
+    if menu == "1":
 
+        boards = board_dao.select_all()
+
+        print()
+        print("번호 제목 작성자 작성일")
+        print("-" * 40)
 
         for board in boards:
-            print(board[0],
-                  board[1],
-                  board[3],
-                  board[4])
-                  
-print("게시판 종료")
+
+            print(
+                board[0],
+                board[1],
+                board[2],
+                board[3]
+            )
+
+    elif menu == "2":
+
+        title = input("제목 : ")
+        content = input("내용 : ")
+        writer = input("작성자 : ")
+
+        board_dao.insert_board(
+            title,
+            content,
+            writer
+        )
+
+        print("등록 완료")
+
+    elif menu == "3":
+
+        num = input("번호 : ")
+
+        board = board_dao.select_one(num)
+
+        if board:
+
+            print()
+            print("번호 :", board[0])
+            print("제목 :", board[1])
+            print("내용 :", board[2])
+            print("작성자 :", board[3])
+            print("작성일 :", board[4])
+            
+            comments = board_dao.select_comments(num)
+        
+            print("\n[댓글 목록]")
+
+            for comment in comments:
+                print()
+                print("댓글번호 :", comment[0])
+                print("작성자 :", comment[3])
+                print("내용 :", comment[2])
+
+    elif menu == "4":
+
+        num = input("삭제 번호 : ")
+
+        board_dao.delete_board(num)
+
+        print("삭제 완료")
+
+    elif menu == "5":
+
+        num = input("수정할 글 번호 : ")
+
+        board = board_dao.select_one(num)
+
+        if board:
+
+            print("현재 제목 : ", board[1])
+            print("현재 내용 : ", board[2])
+
+            title = input("새 제목 : ")
+            content = input("새 내용 : ")
+            
+            board_dao.update_board(
+                num,
+                title,
+                content
+            )
+
+        print("수정 완료")
+
+    elif menu == "6":
+
+        board_id = input("게시글 번호 : ")
+        
+        board = board_dao.select_one(board_id)
+
+        if board:
+
+            content = input("댓글 내용 : ")
+            writer = input("작성자 : ")
+
+            board_dao.insert_comment(
+                board_id,
+                content,
+                writer
+            )
+
+            print("댓글 등록 완료")
+
+        else:
+            print("존재하지 않는 게시글입니다.")
+
+    elif menu == "7":
+
+        board_id = input("게시글 번호 : ")
+
+        comments = board_dao.select_comments(board_id)
+
+        if comments:
+
+            print("\n[댓글 목록]")
+
+            for comment in comments:
+
+                print(
+                    f"댓글번호:{comment[0]}",
+                    f"작성자:{comment[3]}",
+                    f"내용:{comment[2]}"
+                )
+
+            comment_id = input("\n삭제할 댓글 번호 : ")
+
+            board_dao.delete_comment(comment_id)
+
+            print("댓글 삭제 완료")
+
+        else:
+
+            print("댓글이 없습니다.")
+
+    elif menu == "0":
+
+        print("프로그램 종료")
+        break
